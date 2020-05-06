24b71c9e768e384770feb9d1349f1273cf073463
higumachan <harekumo792154@gmail.com>
2020-05-06 11:54:34 +0900

    mixinに隠蔽してみる

--- src/App.vue
+++ src/App.vue
@@ -3,22 +3,19 @@
     <video id="my-video" width="400px" autoplay muted playsinline></video>
     <p id="my-id">{{myId}}</p>
     <textarea id="their-id" v-model="theirId"></textarea>
-    <button id="make-call" @click="makeCall">発信</button>
+    <button id="make-call" @click="makeCall(theirId);">発信</button>
     <video id="their-video" width="400px" autoplay muted playsinline></video>
   </div>
 </template>
 
 <script>
-import Peer from 'skyway-js';
-import {skywayApiKey} from "../config/skyway-config";
+import skywayManager from "@/mixins/skywayManager";
 
 
 export default {
   name: 'App',
   data() {
     return {
-      localStream: null,
-      peer: null,
       myId: "",
       theirId: "",
     }
@@ -26,45 +23,27 @@ export default {
   components: {
   },
   methods: {
-    makeCall() {
-      const mediaConnection = this.peer.call(this.theirId, this.localStream);
-      this.setEventListener(mediaConnection);
-    },
-    setEventListener(mediaConnection) {
+    onGetMediaConnection(mediaConnection) {
       mediaConnection.on('stream', stream => {
         const videoElm = document.getElementById('their-video');
         videoElm.srcObject = stream;
         videoElm.play();
       });
-    }
-  },
-  mounted() {
-    // カメラ映像取得
-    navigator.mediaDevices.getUserMedia({video: true, audio: true})
-      .then( stream => {
-        // 成功時にvideo要素にカメラ映像をセットし、再生
-        const videoElm = document.getElementById('my-video')
-        videoElm.srcObject = stream;
-        videoElm.play();
-        // 着信時に相手にカメラ映像を返せるように、グローバル変数に保存しておく
-        this.localStream = stream;
-      }).catch( error => {
-      // 失敗時にはエラーログを出力
-      console.error('mediaDevice.getUserMedia() error:', error);
-      return;
-    });
-    this.peer = new Peer({
-      key: skywayApiKey,
-      debug: 3
-    });
-    this.peer.on('open', () => {
+    },
+    onPeerOpen() {
       this.myId = this.peer.id;
-    });
-    this.peer.on('call', mediaConnection => {
+    },
+    onPeerCall(mediaConnection) {
       mediaConnection.answer(this.localStream);
-      this.setEventListener(mediaConnection);
-    });
-  }
+      this.onGetMediaConnection(mediaConnection);
+    },
+    onGetUserMediaStream(stream) {
+      const videoElm = document.getElementById('my-video')
+      videoElm.srcObject = stream;
+      videoElm.play();
+    },
+  },
+  mixins: [skywayManager],
 }
 </script>
 
